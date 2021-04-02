a15285cac4719441666f062ae29990b06b88c0ad
ALMA DEBORA MAGUEY PERALTA <alma.magueyp@sat.gob.mx>
2021-04-02 04:35:57 +0200

    Ya se puede iniciar sesión con info hardcodeada

--- src/mods/session.rs
+++ src/mods/session.rs
@@ -1,8 +1,9 @@
 use actix_session::{CookieSession, Session};
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 
-#[derive(Deserialize, Serialize)]
+#[derive(Hash, Eq, PartialEq, Debug, Serialize, Deserialize)]
 pub struct LoginData {
     pub rfc: String,
     pub password: String,
@@ -13,14 +14,49 @@ pub async fn login(rec: web::Json<LoginData>, req: HttpRequest, session: Session
 
     let web::Json(data): web::Json<LoginData> = rec;
 
-    session.set("rfc", data.rfc)?;
-    println!("SESSION value: {:?}", session.get::<String>("rfc").unwrap());
+    if check_password(&data) {
+        session.set("RFC", &data.rfc)?;
+        println!("SESSION value: {:?}", session.get::<String>("RFC").unwrap());
+
+        Ok(HttpResponse::Ok().body("Se inició sesión correctamente"))
+    } else {
+        Ok(HttpResponse::Ok().body("Faiiiiiil"))
+    }
+    // session.set("rfc", data.rfc)?;
+    // println!("SESSION value: {:?}", session.get::<String>("rfc").unwrap());
 
 
+    // println!("{:?}", req);
+
+    // Ok(HttpResponse::Ok().body("hola"))
+
+}
+
+pub fn check_password(data: &LoginData) -> bool {
 
 
-    println!("{:?}", req);
+    let mut datos_login = HashSet::new();
 
-    Ok(HttpResponse::Ok().body("hola"))
+    datos_login.insert(LoginData {
+        rfc: "GOHE".to_string(),
+        password: "AmoALosMichis".to_string(),
+    });
+    datos_login.insert(LoginData {
+        rfc: "MAPA".to_string(),
+        password: "Perritus".to_string(),
+    });
+    datos_login.insert(LoginData {
+        rfc: "A".to_string(),
+        password: "ABC123".to_string(),
+    });
+datos_login.insert(LoginData {
+        rfc: "B".to_string(),
+        password: "123ABC".to_string(),
+    });
 
+    if datos_login.contains(data) {
+        true
+    } else {
+        false
+    }
 }
